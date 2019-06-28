0801ce40cc0301a093c31e76075d705570e32deb
Concourse CI <git_concourse_ci@comcast.com>
2019-06-28 22:18:27 +0000

    [skip ci] bump version from 0.1.2 to 0.1.3

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ import (
 const (
 	applicationName, apiBase = "heimdall", "/api/v1"
 	DEFAULT_KEY_ID           = "current"
-	applicationVersion       = "0.1.2"
+	applicationVersion       = "0.1.3"
 )
 
 type deviceGetter interface {
